2a1851fd0c031be29abc74e4e9a0203f75084267
Владимир Милешко <racerkafa@gmail.com>
2021-10-30 19:09:44 +0300

    *

--- PatientСardWpfApp/Migrations/Configuration.cs
+++ PatientСardWpfApp/Migrations/Configuration.cs
@@ -6,7 +6,7 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "PatientСardWpfApp.Models.AppDBContent";
         }
 
